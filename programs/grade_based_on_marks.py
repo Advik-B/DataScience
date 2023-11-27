fe9543a59abc6450415ab44174a8c072a6000830
Advik <advik.b@gmail.com>
2023-11-27 19:00:03 +0530

    Create grade_based_on_marks.py

---
+++ programs/grade_based_on_marks.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks [0-100]: "))
+
+if marks >= 80:
+    print("You got an A")
+elif marks >= 70:
+    print("You got a B")
+elif marks >= 60:
+    print("You got a C")
+elif marks >= 50:
+    print("You got a D")
+elif marks >= 40:
+    print("You got an E")
+else:
+    print("You got an F")
