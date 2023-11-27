d5f8ef1e6e47d99868ae21d65496205480a3b7d4
Advik <advik.b@gmail.com>
2023-11-27 18:59:37 +0530

    Create search.py

---
+++ programs/search.py
@@ -0,0 +1,8 @@
+s = input("Enter a string: ")
+se = input("Enter a search element: ")
+
+if se in s:
+    print("The search element is in the string")
+else:
+    print("The search element is not in the string")
+
